08e77afef3935951de33e6b7b0acbdcba53377f1
Joey <119094135+JoeyDaHoBo@users.noreply.github.com>
2026-01-04 22:12:41 -0800

    Add files via upload

--- PlayerController.cs
+++ PlayerController.cs
@@ -4,9 +4,9 @@ public class PlayerController : MonoBehaviour
 {
 
 
-    public float speed = 10f;
+    public float speed = 30f;
     public float turnSpeed = 45f;
-    public float jumpHeight = 5f; 
+    public float jumpHeight = 10f; 
 
     private float horizontalInput;
     private float JumpInput; 
@@ -19,6 +19,8 @@ public class PlayerController : MonoBehaviour
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerRb.useGravity = true;
+        playerRb.isKinematic = false;
     }
 
     // Update is called once per frame
@@ -28,22 +30,19 @@ public class PlayerController : MonoBehaviour
         forwardInput = Input.GetAxis("Vertical");
         JumpInput = Input.GetAxis("Jump");
 
-                // first method to move object only on z axis
-        // transform.Translate(0, 0, 1);
 
       
 
                 //move vechile forward per secound
-        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+       transform.Translate(Vector3.forward  * speed * forwardInput * Time.deltaTime);
 
-        transform.Translate(Vector3.down * Time.deltaTime * speed); //gravity effect
+    
 
         //turn vechile left and right
         // transform.Translate(Vector3.right * Time.deltaTime * turnSpeed *horizontalInput);
-        transform.Rotate(Vector3.up,Time.deltaTime * turnSpeed * horizontalInput);
+        transform.Rotate(Vector3.up * turnSpeed * horizontalInput * Time.deltaTime);
         //move vechile up and down
-        transform.Translate(Vector3.up * Time.deltaTime * jumpHeight * JumpInput );
+        transform.Translate(Vector3.up  * jumpHeight * JumpInput * Time.deltaTime);
     }
-
    
 }
